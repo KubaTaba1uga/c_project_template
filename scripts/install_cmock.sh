84a8ed0e4c892d7fd5939b43237f41d76f9e0739
KubaTaba1uga <jakub@buczynski.net.pl>
2023-08-22 13:46:48 +0200

    add validations

--- scripts/install_cmock.sh
+++ scripts/install_cmock.sh
@@ -2,7 +2,23 @@
 
 export CMOCK_DIR=../cmock
 
-// Install cmock and unity
+if ! type "git" > /dev/null; then
+    echo "Git is required to install cmock"
+    exit 1
+fi
+
+
+if ! type "ruby" > /dev/null; then
+    echo "Ruby is required to install cmock"
+    exit 2
+fi
+
+if ! type "bundle" > /dev/null; then
+    echo "Bundler is required to install cmock"
+    exit 2
+fi
+
+# Install cmock and unity locally
 git clone --recursive https://github.com/throwtheswitch/cmock.git $CMOCK_DIR
 cd $CMOCK_DIR
 bundle install
