3dc5c6c0a46fd617f1dc25056e7e5a3e8b55eee9
KubaTaba1uga <jakub@buczynski.net.pl>
2023-09-01 02:33:18 +0200

    refactor

--- test/test_main.d/test_main.c
+++ test/test_main.d/test_main.c
@@ -2,15 +2,9 @@
 
 #include "main.c"
 
-void setUp(void) {
-}
-
-void tearDown(void) {
-}
-
-
-void test_main_(void) {
+void test_main(void) {
   int result = main__();
+
   TEST_ASSERT_EQUAL(result, 0);
   TEST_PASS_MESSAGE("main is working.");
 }
