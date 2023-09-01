8202490de47559f735de4edf4622f2aae5c35fce
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-09-01 21:59:09 +0200

    Update install_ruby.sh

--- scripts/install_ruby.sh
+++ scripts/install_ruby.sh
@@ -17,15 +17,6 @@ case "$OS" in
 	    echo "alias bundle='bundle3.1'" >> ~/.bashrc
 	fi
 	;;
-    *"Ubuntu 22"*)
-  sudo apt update
-	sudo apt install ruby-full
-
-	if ! command -v bundle &> /dev/null
-	then
-	    echo "alias bundle='bundle3.1'" >> ~/.bashrc
-	fi
-	;;
     *)
 	echo "OS not supported"
 	exit 1
